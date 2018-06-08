7c8209ca37f7f39a1244cf120aaaa41c687de018
ebinks <elizabethjbinks@gmail.com>
2018-06-07 21:16:37 -0400

    updated transfer function

--- contracts/Transfers.sol
+++ contracts/Transfers.sol
@@ -1,10 +1,28 @@
 pragma solidity ^0.4.19;
 
-contract Transfers {
-	mapping(address => uint) balance;
+import "./Transfers2.sol";
 
-	function deposit() public payable {
+contract Transfers {
+	mapping (address => uint) public balance;
+	Transfers2 t;
+
+	constructor() public {
+		t = new Transfers2();
+	}
+
+	function transfer(address _addr, uint _value) public {
+		require(balance[msg.sender] >= _value);
+		bytes4 sig = bytes4(keccak256("withdraw(address)"));
+		t.call.value(_value)(sig, _addr);		
+	}
+
+	function myBalance() public returns (uint) {
+		return balance[msg.sender];
+	}
+
+	function deposit() public payable returns (uint) {
 		balance[msg.sender] += msg.value;
+		return balance[msg.sender];
 	}
 
 	function withdraw(address _addr, uint _value) public {
@@ -15,15 +33,6 @@ contract Transfers {
 
 	function withdrawMulti(address[] _addrs, uint _value) public {
 		uint l = _addrs.length;
-		require(balance[msg.sender] >= _value * l);
-		balance[msg.sender] -= _value * l;
-		for (uint i = 0; i < l; i++){
-			_addrs[i].transfer(_value);
-		}
-	}
-
-	function withdrawThree(address[3] _addrs, uint _value) public {
-		uint l = 3;
 		require(balance[msg.sender] >= _value * l);
 		balance[msg.sender] -= _value * l;
 		for (uint i = 0; i < l; i++){
